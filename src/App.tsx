e473d10fa782dadea0615445272bfeb28ee82c9c
hlj <huanglongjin@xiaomai5.com>
2024-06-17 11:02:02 +0800

    test

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import WebApp from '@twa-dev/sdk'
-//import NearbyUsersBot from './NearbyUsersBot'
+import NearbyUsersBot from './NearbyUsersBot'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -18,7 +18,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React1</h1>
+      <h1>Vite + React2</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -40,7 +40,7 @@ function App() {
       <div>NearbyUsersBot test</div>
       <div className='card'>
         NearbyUsersBot
-        {/* <NearbyUsersBot /> */}
+        <NearbyUsersBot />
       </div>
 
     </>
